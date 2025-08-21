adaf57b8e37cffa5d8a086edfe61c341265fbcef
Carolleitwdecoco <lhamamiojo@gmail.com>
2025-08-21 00:36:58 -0300

    Desafio basico

---
+++ app.js
@@ -0,0 +1,19 @@
+//ListaGenerica
+let ListaGenerica = [];
+
+//Linguagens da programação
+
+let linguagensDeProgramação = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+
+//Puxando dados
+
+linguagensDeProgramação.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramação);
+
+//Lista de nomes
+let Nomes = ['Adriano', 'Joaquim', 'Rebecca'];
+
+
+//falando o terceiro nome
+console.log(Nomes[2]);
